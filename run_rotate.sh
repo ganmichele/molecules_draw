e26d8f4652b2dd60b797fff63b8947d642415e1b
Michele Gandolfi <michelegandolfi93@gmail.com>
2022-03-18 12:04:22 +0100

    rotate renamed moldraw & updated example input

--- run_rotate.sh
+++ run_rotate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-./rotate.py example.xyz -R 0 30 30 --order xzy -o example.xyz --tikz test.tex --depth --angles H1C0H2 H2C0H3 --asize 0.5 --connect connectivity_dist.txt
+./moldraw.py example.xyz -R 0 30 30 --order xzy -o example.xyz --tikz test.tex --depth --angles H1C0H2 H2C0H3 --asize 0.5 --connect connectivity_dist.txt
 pdflatex test.tex 1> /dev/null
 
